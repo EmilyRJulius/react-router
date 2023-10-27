5628f792a0962627a8268d3ef4a0b95ddcfeb7b5
Emily Julius <erjulius@tampabay.rr.com>
2023-10-27 11:38:03 -0400

    implemented Chips component

---
+++ src/components/chips.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chips = () => {
+    return <h1>CHIIIIIIIIIIIIPS</h1>
+};
+
+export default Chips;
--- src/components/vending-machine.js
+++ src/components/vending-machine.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Chips from './chips';
 import { BrowserRouter, Route } from "react-router-dom";
 
 const VendingMachine = () => {
-    return <h1>HELLO I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
+    return( 
+    <div>
+    <h1>HELLO I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
+    <BrowserRouter>
+    <Route exact path="/chips">
+        <Chips />
+    </Route>
+    </BrowserRouter>
+    </div>
+    )
 };
 
 export default VendingMachine;
