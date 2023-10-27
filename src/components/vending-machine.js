46091b506ed54aa9d8379fe35153db53f0f632e8
Emily Julius <erjulius@tampabay.rr.com>
2023-10-27 11:32:09 -0400

    added snack components

---
+++ src/components/vending-machine.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+const VendingMachine = () => {
+    return <h1>HELLO I AM A VENDING MACHINE. WHAT WOULD YOU LIKE TO EAT?</h1>
+};
+
+export default VendingMachine;
